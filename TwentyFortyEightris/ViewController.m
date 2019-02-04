8fc4cbdb05befa4ce835cef93e384aa67807360f
Anthony Caligure <acaligu2@slic-41.pods.bu.int>
2019-02-04 16:32:21 -0500

    Rudimentary Swipe Functionality Added

--- TwentyFortyEightris/ViewController.m
+++ TwentyFortyEightris/ViewController.m
@@ -20,6 +20,8 @@ NSMutableArray *gameTiles;
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //Establish Colors for game
+    
     UIColor *backgroundColor = [[UIColor alloc]initWithRed: 176.0/255.0 green: 210.0/255.0 blue: 202.0/255.0 alpha: 1.0];
     UIColor *textColor = [[UIColor alloc]initWithRed:119.0/255.0 green: 138.0/255.0 blue: 152.0/255.0 alpha: 1.0];
     UIColor *tileColor = [[UIColor alloc]initWithRed:221.0/255.0 green: 221.0/255.0 blue: 221.0/255.0 alpha: 1.0];
@@ -28,9 +30,25 @@ NSMutableArray *gameTiles;
     self.view.backgroundColor = backgroundColor;
     [gameTitle setTextColor: textColor];
     
+    //Choose first two tiles that will contain vaules
+    
+    NSInteger firstTile = arc4random() % 16;
+    NSInteger secondTile = arc4random() % 16;
+    
+    //Ensure the same tile isn't picked
+    if(secondTile == firstTile){
+        
+        secondTile = arc4random() % 16;
+        
+    }
+    
+    //Loop through array of labels and set all to color
     for (UILabel *tile in gameTiles) {
         [tile setBackgroundColor:tileColor];
     }
+    
+    [gameTiles[firstTile] setText: @"2"];
+    [gameTiles[secondTile] setText: @"2"];
         
 }
 
@@ -39,6 +57,42 @@ NSMutableArray *gameTiles;
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)swipeTest:(id)sender {
+    
+    UISwipeGestureRecognizer *senderDir = (UISwipeGestureRecognizer *)sender;
+    
+    UISwipeGestureRecognizerDirection dir = senderDir.direction;
+    
+    switch(dir){
+    
+        case(UISwipeGestureRecognizerDirectionUp):
+            
+            [swipeTest setText: @"Swiped Up"];
+            break;
+            
+        case(UISwipeGestureRecognizerDirectionDown):
+            
+            [swipeTest setText: @"Swiped Down"];
+            break;
+            
+        case(UISwipeGestureRecognizerDirectionRight):
+            
+            [swipeTest setText: @"Swiped Right"];
+            break;
+            
+        case(UISwipeGestureRecognizerDirectionLeft):
+            
+            [swipeTest setText: @"Swiped Left"];
+            break;
+            
+        default:
+            
+            break;
+            
+            
+    }
+    
 }
 
 @end
